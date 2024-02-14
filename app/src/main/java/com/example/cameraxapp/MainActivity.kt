13850652afa0921065d048e4ed165b491df92bf5
Raj <raj.android@necun.in>
2024-02-14 17:20:47 +0530

    logout added

--- app/src/main/java/com/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.cameraxapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogout: CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogout = findViewById(R.id.btnLogout)
+        btnLogout.setOnClickListener {
+            startActivity(Intent(this@MainActivity,RegistrationActivity::class.java))
+        }
     }
 }
