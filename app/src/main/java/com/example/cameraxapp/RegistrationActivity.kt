8b8897418b812cb818ea3942504eedd05f823d9c
Raj <raj.android@necun.in>
2024-02-14 12:06:34 +0530

    signupscreen

--- app/src/main/java/com/example/cameraxapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/cameraxapp/RegistrationActivity.kt
@@ -2,10 +2,25 @@ package com.example.cameraxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var txtCompany: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        txtCompany = findViewById(R.id.textView)
+        val spannable = SpannableStringBuilder(getString(R.string.company_name))
+        spannable.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            0,
+            2,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        txtCompany.text = spannable
     }
 }
