7a292ebf75136a5e82e283799308707154a47219
Raj <raj.android@necun.in>
2024-02-14 16:40:59 +0530

    otpscreen added

--- app/src/main/java/com/example/cameraxapp/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/cameraxapp/ForgetPasswordActivity.kt
@@ -1,11 +1,32 @@
 package com.example.cameraxapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.ImageView
+import android.widget.TextView
 
 class ForgetPasswordActivity : AppCompatActivity() {
+    private lateinit var btnBack: ImageView
+    private lateinit var txtCompanyName: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+        txtCompanyName = findViewById(R.id.textView)
+        val spannable = SpannableStringBuilder(getString(R.string.company_name))
+        spannable.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            0,
+            2,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        txtCompanyName.text = spannable
+        btnBack.setOnClickListener {
+            startActivity(Intent(this@ForgetPasswordActivity,SignInActivity::class.java))
+            finishAffinity()
+        }
     }
 }
--- app/src/main/java/com/example/cameraxapp/OTPActivity.kt
+++ app/src/main/java/com/example/cameraxapp/OTPActivity.kt
@@ -1,11 +1,32 @@
 package com.example.cameraxapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.ImageView
+import android.widget.TextView
 
 class OTPActivity : AppCompatActivity() {
+    private lateinit var txtCompanyName: TextView
+    private lateinit var btnBack: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otpactivity)
+        txtCompanyName = findViewById(R.id.textView)
+        val spannable = SpannableStringBuilder(getString(R.string.company_name))
+        spannable.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            0,
+            2,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        txtCompanyName.text = spannable
+        btnBack.setOnClickListener {
+            startActivity(Intent(this@OTPActivity,SignUpActivity::class.java))
+            finishAffinity()
+        }
     }
 }
